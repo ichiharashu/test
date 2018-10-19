aef93fd7807e0463313746bd0d03aaf182fdb5d0
arxxxxxx <arimaore@gmail.com>
2018-10-19 21:40:09 +0900

    10/19

---
+++ Android/src/SmartPhone.java
@@ -0,0 +1,18 @@
+
+public class SmartPhone extends Mp3Player{
+	public void call(){
+		System.out.println("電話");
+	}
+
+	public void mail(){
+		System.out.println("メール");
+	}
+
+	public void map(){
+		System.out.println("マップ");
+	}
+
+	public void chrome(){
+		System.out.println("クローム");
+	}
+}
--- Cupsule/src/Person.java
+++ Cupsule/src/Person.java
@@ -10,4 +10,9 @@ public class Person{
 	public void setName(String name){
 		this.name=name;
 	}
+
+	public Person(String name,int age){
+		this.name=name;
+		this.age=age;
+	}
 }
